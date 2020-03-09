fa636ff22a51febf1465ed69e02816b3845bd935
oksam90 <HP@192.168.56.1>
2020-03-10 00:32:02 +0100

    premier commit

---
+++ GestionProduit/src/test/java/nn/oksam90/GestionProduit/GestionProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package nn.oksam90.GestionProduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
